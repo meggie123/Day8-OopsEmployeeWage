c07bdf40cb239f1b5547bdeed856044d34710b71
meggie123 <meghanabura@gmail.com>
2022-06-02 14:10:58 +0530

    Welcome To Employee Wage Program

---
+++ main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Day8EmpWage;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+    }
+}
