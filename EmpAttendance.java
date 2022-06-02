2313b681ceb61756986f8a3432a64b2c9f0681b6
meggie123 <meghanabura@gmail.com>
2022-06-02 14:30:01 +0530

    Checking Employee Attendance

---
+++ EmpAttendance.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.Day8EmpWage;
+
+public class EmpAttendance {
+    public static void main (String[] args) {
+        int IS_FULL_TIME = 1;
+
+        int empCheck = (int)Math.floor(Math.random() * 10 ) % 2;
+
+        if(empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present.");
+        }
+        else
+            System.out.println("Employee is Absent.");
+    }
+}
+
+
